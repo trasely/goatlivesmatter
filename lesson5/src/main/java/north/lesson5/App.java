ccc35208a1ed21984968e1a579d9df60ba7a00eb
Trasely <david.a.thomas1003@gmail.com>
2019-10-07 09:08:37 -0500

    adding MD5

--- lesson5/src/main/java/north/lesson5/App.java
+++ lesson5/src/main/java/north/lesson5/App.java
@@ -9,5 +9,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        String password = "123456";
+        System.out.println("MD5: " + MD5encryption.MD5(password));
     }
 }
