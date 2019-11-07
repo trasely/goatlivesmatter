3548bdea8f7a1bd6870eb1868b29ade5a971fb8a
ioexcept <ioexcept@gmail.com>
2019-11-07 11:48:55 -1000

    added file to setup java source structure

---
+++ lesson8/src/main/resources/efn/lesson8/web/HelloWorld.java
@@ -0,0 +1,15 @@
+package efn.lesson8.web;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String sayHello() { return "Hello World!"; }
+	
+	public static void main(String[] args) {
+		out.println(new HelloWorld().sayHello());
+	}
+
+}
